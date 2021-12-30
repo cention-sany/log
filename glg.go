1b4b9b173b6e3651c9261b250975c8e247fb82a3
Sany Liew <sany.liew@cention.se>
2021-12-30 20:38:30 +0800

    Use log.Default To Tap Into Stdlib Default Log

--- glg.go
+++ glg.go
@@ -117,69 +117,11 @@ func (nolog) SetFlags(int) {}
 func (nolog) Flags() int { return 0 }
 
 // access to stdlib log
-type stdLibLog struct{}
+type stdLibLog struct {
+	*log.Logger
+}
 
 // StdLib allows access to global singleton standard library log object.
 func StdLib() IFLogger {
-	return &stdLibLog{}
-}
-
-func (stdLibLog) Fatal(v ...interface{}) {
-	log.Fatal(v...)
-}
-
-func (stdLibLog) Fatalf(format string, v ...interface{}) {
-	log.Fatalf(format, v...)
-}
-
-func (stdLibLog) Fatalln(v ...interface{}) {
-	log.Fatalln(v...)
-}
-
-func (stdLibLog) Panic(v ...interface{}) {
-	log.Panic(v...)
-}
-
-func (stdLibLog) Panicf(format string, v ...interface{}) {
-	log.Panicf(format, v...)
-}
-
-func (stdLibLog) Panicln(v ...interface{}) {
-	log.Panicln(v...)
-}
-
-func (stdLibLog) Prefix() string {
-	return log.Prefix()
-}
-
-func (stdLibLog) Print(v ...interface{}) {
-	log.Print(v...)
-}
-
-func (stdLibLog) Printf(format string, v ...interface{}) {
-	log.Printf(format, v...)
-}
-
-func (stdLibLog) Println(v ...interface{}) {
-	log.Println(v...)
-}
-
-func (stdLibLog) SetPrefix(prefix string) {
-	log.SetPrefix(prefix)
-}
-
-func (stdLibLog) SetOutput(w io.Writer) {
-	log.SetOutput(w)
-}
-
-func (stdLibLog) Output(calldepth int, s string) error {
-	return log.Output(calldepth, s)
-}
-
-func (stdLibLog) SetFlags(f int) {
-	log.SetFlags(f)
-}
-
-func (stdLibLog) Flags() int {
-	return log.Flags()
+	return stdLibLog{Logger: log.Default()}
 }
